ad2ea1af3b75b4568f7b01c3ac4cb76d1283c82e
Aman1727 <amangoyal1727@gmail.com>
2021-11-20 20:46:10 +0530

    URL Session Request Made

---
+++ youtube-iOS/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  youtube-iOS
+//
+//  Created by Aman on 20/11/21.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
--- youtube-iOS/ViewController.swift
+++ youtube-iOS/ViewController.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var model = Model()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        model.getVideos()
     }
 
 
